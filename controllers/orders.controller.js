b4eaa98425b4ceb1f5d87a3d802f2f0626b7174a
anas derk <anas.derk2020@gmail.com>
2023-12-09 10:24:14 -0800

    Continue

--- controllers/orders.controller.js
+++ controllers/orders.controller.js
@@ -11,10 +11,17 @@ async function postNewOrder(req, res) {
 
 async function postNewPayment(req, res) {
     try{
-        const result = await postNewOrder(req.body);
+        const { post } = require("axios");
+        const res = await post("https://sandboxapi.upayments.com/api/v1/charge", req.body, {
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${process.env.UPAYMENTS_TOKEN}`
+            },
+        })
         await res.json(result);
     }
     catch(err) {
+        console.log(err);
         await res.status(500).json(err);
     }
 }
@@ -22,6 +29,7 @@ async function postNewPayment(req, res) {
 async function putOrder(req, res) {
     try{
         const orderId = req.params.orderId;
+        console.log(orderId);
         const newOrderDetails = req.body;
         if (!orderId) await res.status(400).json("Please Send Order Id !!");
         else {
