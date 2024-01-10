33131e5245b95f8bec471126a27860b4f90b2e9a
anas derk <anas.derk2020@gmail.com>
2024-01-10 12:21:12 -0800

    Continue

--- controllers/orders.controller.js
+++ controllers/orders.controller.js
@@ -74,6 +74,7 @@ async function getOrderDetails(req, res) {
 }
 
 async function postNewOrder(req, res) {
+    console.log(req.body);
     try{
         const { postNewOrder } = require("../models/orders.model");
         const result = await postNewOrder(req.body);
