847776d23d1ca35262acca7d09a0ab58d714eed7
anas derk <anas.derk2020@gmail.com>
2024-03-08 19:02:43 -0800

    Continue

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -39,18 +39,8 @@ async function getAdminLogin(req, res) {
 
 async function getAdminUserInfo(req, res) {
     try{
-        const token = req.headers.authorization;
-        const checkResult = checkIsExistValueForFieldsAndDataTypes([
-            { fieldName: "JWT", fieldValue: token, dataType: "string", isRequiredValue: true },
-        ]);
-        if (checkResult.error) {
-            await res.status(400).json(checkResult);
-            return;
-        }
-        const { verify } = require("jsonwebtoken");
-        const data = verify(token, process.env.secretKey);
         const { getAdminUserInfo } = require("../models/admin.model");
-        await res.json(await getAdminUserInfo(data._id));
+        await res.json(await getAdminUserInfo(req.data._id));
     }
     catch(err){
         await res.status(500).json(getResponseObject(err.message, true, {}));
--- routes/admin.router.js
+++ routes/admin.router.js
@@ -2,8 +2,10 @@ const adminRouter = require("express").Router();
 
 const adminController = require("../controllers/admin.controller");
 
+const { validateJWT } = require("../middlewares/global.middlewares");
+
 adminRouter.get("/login", adminController.getAdminLogin);
 
-adminRouter.get("/user-info", adminController.getAdminUserInfo);
+adminRouter.get("/user-info", validateJWT, adminController.getAdminUserInfo);
 
 module.exports = adminRouter;
