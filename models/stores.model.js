9880a180331e49ff9190505dd50535a10e9f5353
anas derk <anas.derk2020@gmail.com>
2024-04-16 16:19:28 -0700

    Sort The Code

--- models/stores.model.js
+++ models/stores.model.js
@@ -6,24 +6,24 @@ const { storeModel, adminModel, categoryModel, productModel, brandModel } = requ
 
 const { hash } = require("bcryptjs");
 
-async function getAllStoresInsideThePage(pageNumber, pageSize, filters) {
-    try {
-        return {
-            msg: `Get All Stores Inside The Page: ${pageNumber} Process Has Been Successfully !!`,
-            error: false,
-            data: await storeModel.find(filters).skip((pageNumber - 1) * pageSize).limit(pageSize).sort({ creatingOrderDate: -1 }),
-        }
-    } catch (err) {
-        throw Error(err);
-    }
-}
-
 async function getStoresCount(filters) {
     try {
         return {
             msg: "Get Stores Count Process Has Been Successfully !!",
             error: false,
             data: await storeModel.countDocuments(filters),
+        }
+    } catch (err) {
+        throw Error(err);
+    }
+}
+
+async function getAllStoresInsideThePage(pageNumber, pageSize, filters) {
+    try {
+        return {
+            msg: `Get All Stores Inside The Page: ${pageNumber} Process Has Been Successfully !!`,
+            error: false,
+            data: await storeModel.find(filters).skip((pageNumber - 1) * pageSize).limit(pageSize).sort({ creatingOrderDate: -1 }),
         }
     } catch (err) {
         throw Error(err);
